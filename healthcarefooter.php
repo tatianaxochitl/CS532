e1eed9e659487b97e323df348cb1803d405df0f9
Tatiana Chavez <tatianaxochitl@Citlalincue.local>
2018-11-29 15:36:44 -0800

    hey added stuff

---
+++ healthcarefooter.php
@@ -0,0 +1,19 @@
+
+
+</TD>
+</TR>
+<TR><TD><img src="spacer.gif" alt="" width=140 height=1></TD><TD><img src="spacer.gif" alt="" width=520 height=1></TD></TR>
+<TR>
+<TD colspan=2>
+<hr size=1 noshade size=1>
+<span class="small">copyright &copy; 2018 CCTT Healthcare</span>
+<P>
+
+</TABLE>
+
+
+</TD></TR></TABLE>
+
+
+</BODY>
+</HTML>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php $title="Overview"?>
+<?php include ("healthcareheader.php") ?>
+
+
+<h1>Welcome to CCTT HealthCare</h1>
+
+Please choose from the menus above 
+
+
+
+                                                                 
+<?php include ("healthcarefooter.php") ?>
