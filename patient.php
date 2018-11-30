a5de324ab0fff3354e26eec225c8be87b85c3778
Jose Carlos Gomez <josecarlosgomez69gmail.com>
2018-11-29 17:01:45 -0800

    Added forms to the patient record, still has no functionality though

--- patient.php
+++ patient.php
@@ -6,8 +6,48 @@
 <body>
 <h3>Patient Records</h1>
 <p>Enter patent information below and hit enter</p>
-<table>
-</table>
+<form action="/action_page.php">
+  First name:<br>
+  <input type="text" name="firstname" placeholder="Ex. John"><br>
+
+  Last name:<br>
+  <input type="text" name="lastname" placeholder="Ex. Doe"><br><br>
+
+  Patient ID:<br>
+  <input type="text" name="patientID" placeholder="Ex. 123456"><br>
+
+  Address:<br>
+  <input type="text" name="address" placeholder="Ex. 123 Main St."><br>
+
+  City:<br>
+  <input type="text" name="city" placeholder="Ex. San Diego"><br>
+
+  State:<br>
+  <input type="text" name="state" placeholder="Ex. CA"><br><br>
+
+  Date of Birth:<br>
+  <input type="text" name="dob" placeholder="Ex. 01/01/1990"><br><br>
+
+<p>Gender:</p>
+
+<div>
+  <input type="radio" id="male" name="gender""
+         checked>
+  <label for="male">Male</label>
+</div>
+
+<div>
+  <input type="radio" id="female" name="gender">
+  <label for="female">Female</label>
+</div>
+
+<div>
+  <input type="radio" id="other" name="gender">
+  <label for="other">Other</label>
+</div><br><br>
+
+  <input type="submit" value="Submit">
+</form>
 
 </body>
 
