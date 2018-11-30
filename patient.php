cb763cefa35b233f7333c771cb0375c5825b7423
Jose Carlos Gomez <josecarlosgomez69gmail.com>
2018-11-29 16:57:58 -0800

    Added forms to the patient record, still has no functionality though

--- patient.php
+++ patient.php
@@ -3,7 +3,13 @@
 
 <?php include ("healthcareheader.php") ?>
 
+<body>
+<h3>Patient Records</h1>
+<p>Enter patent information below and hit enter</p>
+<table>
+</table>
 
+</body>
 
 
 <?php include ("healthcarefooter.php") ?>
