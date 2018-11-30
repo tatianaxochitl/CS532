7dba17bbd6c90e4192a35492e7a82554f6a0c6a2
Carlitos5963 <carlos.gomez5963@gmail.com>
2018-11-29 17:55:38 -0800

    Update healthcareheader.php

--- healthcareheader.php
+++ healthcareheader.php
@@ -46,3 +46,32 @@
 <table border="1" bgcolor="000000" cellpadding="1" cellspacing="0" align="center">
 <tr>
 <td valign="top" align="center">
+
+
+<table border="0" bgcolor="ffffff" width="1000" align="center" cellpadding="10" cellspacing="0">
+<tr>
+<td colspan="2">
+
+<a href="index.php"><img src="CCTT Banner.jpg" alt="CCTT Healthcare" border=0></a>
+</td>
+</tr>
+
+<td valign="top">
+
+	<table border="0" cellpadding="6" cellspacing="1" align="left" bgcolor="ffffff" width="500">
+	<td bgcolor="#00008b">
+	<a href="patient.php" class="mainnav"><b>Patient Record</b></a>
+	</td>
+	<td bgcolor="#00008b">
+	<a href="scheduler.php"  class="mainnav"><b>Physician Scheduler</b></a>
+	</td>
+	<td bgcolor="#00008b">
+	<a href="insurance.php"  class="mainnav"><b>Insurance Billing</b></a>
+	</td>
+	<table>
+</td>
+
+<tr><td valign="top">
+
+
+
