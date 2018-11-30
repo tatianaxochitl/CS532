b8ec5cd16161a9bcae3450143e093e991f1c4fed
Carlitos5963 <carlos.gomez5963@gmail.com>
2018-11-29 17:53:50 -0800

    Update healthcareheader.php

--- healthcareheader.php
+++ healthcareheader.php
@@ -7,6 +7,38 @@
 <meta name="robots" content="INDEX, FOLLOW">
 </head>
 
+	<style type="text/css">
+	<!--
+	    a:link { color:#0307BA; text-decoration:underline; font-size : 11px; font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;}
+		a:hover {  color: #B38303; text-decoration:underline; font-size : 11px; font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;}
+
+
+		p { font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;	font-size: 11px; color:#000000}
+		td { font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;	font-size: 11px; color:#000000}
+
+ 		H2 { color:#5E5A54; text-decoration:none; font-size : 16px; font-family: Verdana, Geneva, Arial, Helvetica, sans-serif}
+ 		H3 { color:#5E5A54; text-decoration:none; font-size : 13px; font-family: Verdana, Geneva, Arial, Helvetica, sans-serif}
+
+		ul { font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;	font-size: 11px; color:#000000}
+
+		th
+   		{
+   		font-family: Verdana, Arial, Helvetica, sans-serif;
+   		font-size: 12px;
+   		color: #745C41;
+   		font-weight: bold;
+   		}
+
+		.red { font-family: Verdana, Geneva, Arial, Helvetica, sans-serif;font-size: 13px; color:#FF0000}
+
+		.mainnav {font-family: Verdana, Geneva, Arial, Helvetica, sans-serif; font-size: 13px; color:#FFFFFF;}
+		a.mainnav {text-decoration:none; color:#FFFFFF; font-family: Verdana, Geneva, Arial, Helvetica, sans-serif; font-size: 13px;}
+		a.mainnav:visited {text-decoration:none; color:#FFFFFF; font-family: Verdana, Geneva, Arial, Helvetica, sans-serif; font-size: 13px;}
+		a.mainnav:hover {text-decoration:underline; color:#FFFFFF; font-family: Verdana, Geneva, Arial, Helvetica, sans-serif; font-size: 13px;}
+
+	-->
+	</style>
+
 
 <body background="hospital-new.jpg"  marginwidth="0" leftmargin="0" >
 
@@ -14,32 +46,3 @@
 <table border="1" bgcolor="000000" cellpadding="1" cellspacing="0" align="center">
 <tr>
 <td valign="top" align="center">
-
-
-<table border="0" bgcolor="ffffff" width="700" align="center" cellpadding="10" cellspacing="0">
-<tr>
-<td>
-
-<a href="index.php"><img src="CCTT Banner.jpg" alt="CCTT Healthcare" border=0></a>
-</td>
-</tr>
-
-<td valign="top">
-
-	<table border="0" cellpadding="6" cellspacing="1" align="left" bgcolor="ffffff" width="700">
-	<td bgcolor="#00008b">
-	<a href="patient.php" class="mainnav"><b>Patient Record</b></a>
-	</td>
-	<td bgcolor="#00008b">
-	<a href="scheduler.php"  class="mainnav"><b>Physician Scheduler</b></a>
-	</td>
-	<td bgcolor="#00008b">
-	<a href="insurance.php"  class="mainnav"><b>Insurance Billing</b></a>
-	</td>
-	<table>
-</td>
-
-<tr><td align = "center" valign="top" colspan ="3">
-
-
-
